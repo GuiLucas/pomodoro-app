94445d147b187f870e5df8eb4f943071d90ab675
Guilherme Lucas <guilherme.lucas@avlino.com>
2023-02-08 21:56:31 +0000

    feat: added components to main page.

--- src/pages/CallbackPage.tsx
+++ src/pages/CallbackPage.tsx
@@ -1,19 +1,28 @@
-import { useState } from 'react'
-import { PageLayout, WelcomeModal } from '../components';
-import { useUserEmail } from '../store';
+import { Box, Center, Flex } from '@mantine/core';
+import { LoaderOverlay, PageLayout, WelcomeForm, TodoList } from '../components';
+import {  } from '../components/TodoList';
+import {  useTodos } from '../lib';
 
 export function CallbackPage() {
-    const [isModalOpen, setIsModalOpen] = useState(true)
 
-    const userEmail = useUserEmail()
+    const { isLoading, error, data } = useTodos(1)
+
+    if(isLoading) return <LoaderOverlay /> 
+
+    if(error) return <p>There was an error with retrieving data: {error?.message}</p>
 
     return (
         <PageLayout>
-            <h1>{userEmail}</h1>
-            <WelcomeModal 
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-            />
+            <Box>
+                <Center>
+                    <Box>
+                        <Flex justify='center'>
+                            <WelcomeForm />
+                        </Flex>
+                        <TodoList todos={data} />
+                    </Box>
+                </Center>
+            </Box>
         </PageLayout>
     )
 }
