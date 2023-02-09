a7fbf673ac4413b28fa619ffae650493f61fa2b5
Guilherme Lucas <guilherme.lucas@avlino.com>
2023-02-09 15:49:49 +0000

    refactor: types

--- src/types/todos.ts
+++ src/types/todos.ts
@@ -1,28 +1,4 @@
-import type * as Core from '../types'
-
-export type User = {
-    id: number
-    name: string
-    username: string
-    email: string
-    address: {
-        street: string
-        suite: string
-        city: string
-        zipcode: string
-        geo: {
-            lat: string
-            lng: string
-        }
-    }
-    phone: string
-    website: string
-    company: {
-      name: string
-      catchPhrase: string
-      bs: string
-    }
-}
+import type * as Core from '.'
 
 export type Todo = {
     userId: number
