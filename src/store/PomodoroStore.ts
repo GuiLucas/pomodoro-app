31612b82b239aa0c189c3e20fb13f1e44d5f6f76
Guilherme Lucas <guilherme.lucas@avlino.com>
2023-02-09 15:49:39 +0000

    feat: added more actions to store.

--- src/store/PomodoroStore.ts
+++ src/store/PomodoroStore.ts
@@ -1,32 +1,64 @@
-import { create } from 'zustand'
 import type * as Core from '../types'
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import { toggleCompleted } from '../lib'
 
 type PomodoroState = {
-    userEmail: Core.Optional<string>
-    // todos: Todo[]
+    userEmail: string
+    todos: Core.Todo[]
+    activeTodo: Core.Optional<number>
+    completedPomodoros: number
 }
 
 type PomodoroActions = {
     actions: {
         setUserEmail: (userEmail: string) => void
+        setTodos: (todos: Core.Todo[]) => void
+        setActiveTodo: (todoId: Core.Optional<number>) => void
+        toggleCompleted: (todoId: number) => void
+        resetCompletedPomodoro: () => void
     }
 }
 
 type PomodoroStore = PomodoroState & PomodoroActions
 
 const initialState: PomodoroState = {
-    userEmail: undefined
+    userEmail: '',
+    todos: [],
+    activeTodo: undefined,
+    completedPomodoros: 0
 }
 
-const usePomodoroStore = create<PomodoroStore>()((set, get) => ({
-    ...initialState,
-    actions: {
-        setUserEmail:
-            (userEmail) => set({ userEmail }),
-    }
-}))
+const usePomodoroStore = create<PomodoroStore>()(
+    persist(
+        (set, get) => ({
+            ...initialState,
+            actions: {
+                setUserEmail:
+                    (userEmail) => set({ userEmail }),
+                setTodos: 
+                    (todos) => set({ todos }),
+                setActiveTodo:
+                    (todoId) => set({ activeTodo: todoId}),
+                toggleCompleted: 
+                    (todoId) => set({ 
+                        todos: toggleCompleted(get().todos, todoId),
+                        completedPomodoros: get().completedPomodoros + 1
+                    }),
+                resetCompletedPomodoro: 
+                    () => set({ completedPomodoros: 0 }),
+            }
+        }),
+        {
+            name: 'pomodoro-user-email',
+            partialize: (state) => ({ userEmail: state.userEmail }),
+        }
+    )
+)
 
 export const useUserEmail = () => usePomodoroStore((state) => state.userEmail)
-
-export const usePomodorosActions = () => usePomodoroStore((state) => state.actions)
+export const useTodosFromStore = () => usePomodoroStore((state) => state.todos)
+export const useActiveTodo = () => usePomodoroStore((state) => state.activeTodo)
+export const useCompletedPomodoros = () => usePomodoroStore((state) => state.completedPomodoros)
+export const useStoreActions = () => usePomodoroStore((state) => state.actions)
 
